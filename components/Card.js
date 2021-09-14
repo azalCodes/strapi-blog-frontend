fabfd044c0aeb864602183bbe852c213360baa81
Azal Abedi <azal.abedi@gmail.com>
2021-09-14 16:13:01 -0600

    build Card.js

---
+++ components/Card.js
@@ -0,0 +1,32 @@
+/* 
+  Just a simple component that will be used for displaying the article on the main page. 
+  It is using the Image component as well 
+*/
+
+import React from "react";
+import Link from "next/link";
+import Image from "./image";
+
+const Card = ({ article }) => {
+  return (
+    <Link as={`/article/${article.slug}`} href="/article/[id]">
+      <a className="uk-link-reset">
+        <div className="uk-card uk-card-muted">
+          <div className="uk-card-media-top">
+            <Image image={article.image} />
+          </div>
+          <div className="uk-card-body">
+            <p id="category" className="uk-text-uppercase">
+              {article.category.name}
+            </p>
+            <p id="title" className="uk-text-large">
+              {article.title}
+            </p>
+          </div>
+        </div>
+      </a>
+    </Link>
+  );
+};
+
+export default Card;

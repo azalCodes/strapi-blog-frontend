fc3cc51d0fe88472582317b4907824eac726c798
Azal Abedi <azal.abedi@gmail.com>
2021-09-15 10:20:37 -0600

    fix Layout render null issue

--- components/Layout.js
+++ components/Layout.js
@@ -1,10 +1,12 @@
 import Nav from './Nav';
 
 const Layout = ({ children, categories, seo }) => {
-  <>
-    <Nav categories={categories} />
-    {children}
-  </>
+  return (
+    <>
+      <Nav categories={categories} />
+      {children}
+    </>
+  );
 };
 
 export default Layout;
